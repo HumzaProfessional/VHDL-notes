940e77a7617fc3510b67d1963a8d6d2d392a42f4
Humza Rana <142959387+HumzaProfessional@users.noreply.github.com>
2025-01-29 14:56:46 -0500

    Update and rename 4_referee.vhdl to VOTE_SELECTOR.vhdl

---
+++ lab1/VOTE_SELECTOR.vhdl
@@ -0,0 +1,17 @@
+
+entity VOTE_SELECTOR is
+  port(
+    head_ref: in std_logic;
+    ref_1 : in std_logic;
+    ref_2 : in std_logic;
+    ref_2: in std_logic;
+    vote: out std_logic
+
+    );
+
+end VOTE_SELECTOR
+
+  signal 
+archtecticture ARCH_VOTE_SELECTOR of VOTE_COUNTER is 
+  begin
+    
