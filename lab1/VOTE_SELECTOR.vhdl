40700577f0ee6514c6c45ffcccc3157b0b667871
Humza Rana <142959387+HumzaProfessional@users.noreply.github.com>
2025-01-31 09:17:36 -0500

    Update VOTE_SELECTOR.vhdl

--- lab1/VOTE_SELECTOR.vhdl
+++ lab1/VOTE_SELECTOR.vhdl
@@ -18,7 +18,7 @@ end VOTE_SELECT
     signal vote_count : integer range 0 to 4;
     signal votes      : std_logic_vector(3 downto 0);
 begin
-    votes <= head_ref & ref_1 & ref_2 & ref_3;
+    votes <= head_ref & ref_1 & ref_2 & ref_3; -- each vote is assigned to each bit of votes vector.
 
     -- Count the number of '1's
   process(votes)
@@ -31,13 +31,30 @@ begin
 
 
   
-    -- 
+    with votes select
+        vote <= '0' when "0000",  -- 0 '1's: Majority '0'
+                '0' when "0001",  --
+                '0' when "0010",  
+                head_ref when "0011", -- 2 '1's: Tie, use head_ref's vote
+                '0' when "0100", 
+                head_ref when "0101", -- 2 '1's: Tie
+                head_ref when "0110", -- 2 '1's: Tie
+                '1' when "0111",  
+                '0' when "1000",  
+                head_ref when "1001", -- 2 '1's: Tie
+                head_ref when "1010", -- 2 '1's: Tie
+                '1' when "1011",  -- 
+                head_ref when "1100", -- 2 '1's: Tie
+                '1' when "1101",  
+                '1' when "1110",  
+                '1' when "1111",  -- 4 '1's: Majority '1'
+                'X' when others;    -- Undefined case
     -- Determine the outcome using with-select
-    with vote_count select
-        vote <= '1' when 3 | 4,                -- Majority '1'
-                head_ref when 2,               -- Tie: head_ref's vote
-                '0' when 0 | 1,                -- Majority '0'
-                'X' when others;               -- Undefined (shouldn't occur)
+ --   with vote_count select
+   --     vote <= '1' when 3 | 4,                -- Majority '1'
+     --           head_ref when 2,               -- Tie: head_ref's vote
+       --         '0' when 0 | 1,                -- Majority '0'
+         --       'X' when others;               -- Undefined (shouldn't occur)
 end Behavioral;
   
  
