9eeeace0a68ddde9347fecb082ad280b02a6436b
Humza Rana <142959387+HumzaProfessional@users.noreply.github.com>
2025-01-30 21:36:18 -0500

    Update Basic vhdl program.vhdl

--- Basic vhdl program.vhdl
+++ Basic vhdl program.vhdl
@@ -20,3 +20,42 @@ led1 <= sw1 or sw2 or sw3;
 led2 <= (sw1 and sw2) or (sw1 and sw3) or (sw2 and sw3);
 led3 <= sw1 and sw2 and sw3;
 end Intro_ARCH;
+
+### test bench
+
+SWITCH_DRIVER: process is
+begin
+sw1 <= not ACTIVE;
+sw2 <= not ACTIVE;
+sw3 <= not ACTIVE;
+wait for 100ns;
+sw1 <= ACTIVE;
+sw2 <= not ACTIVE;
+sw3 <= not ACTIVE;
+wait for 100ns;
+sw1 <= not ACTIVE;
+sw2 <= ACTIVE;
+sw3 <= not ACTIVE;
+wait for 100ns;
+sw1 <= ACTIVE;
+sw2 <= ACTIVE;
+sw3 <= not ACTIVE;
+wait for 100ns;
+sw1 <= not ACTIVE;
+sw2 <= not ACTIVE;
+sw3 <= ACTIVE;
+wait for 100ns;
+sw1 <= ACTIVE;
+Tippens
+sw2 <= not ACTIVE;
+sw3 <= ACTIVE;
+wait for 100ns;
+sw1 <= not ACTIVE;
+sw2 <= ACTIVE;
+sw3 <= ACTIVE;
+wait for 100ns;
+sw1 <= ACTIVE;
+sw2 <= ACTIVE;
+sw3 <= ACTIVE;
+wait;
+end process;
