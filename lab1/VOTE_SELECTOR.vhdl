80c1552c0bf2c95b49b831d9e8130464ba3ef709
Humza Rana <142959387+HumzaProfessional@users.noreply.github.com>
2025-01-30 21:33:07 -0500

    Update VOTE_SELECTOR.vhdl

--- lab1/VOTE_SELECTOR.vhdl
+++ lab1/VOTE_SELECTOR.vhdl
@@ -1,5 +1,8 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
 
-entity VOTE_SELECTOR is
+entity VOTE_SELECT is
   port(
     head_ref: in std_logic;
     ref_1 : in std_logic;
@@ -9,9 +12,33 @@ entity VOTE_SELECTOR is
 
     );
 
-end VOTE_SELECTOR
+end VOTE_SELECT
 
-  signal 
-archtecticture ARCH_VOTE_SELECTOR of VOTE_COUNTER is 
-  begin
+ architecture Behavioral of VOTE_SELECT is
+    signal vote_count : integer range 0 to 4;
+    signal votes      : std_logic_vector(3 downto 0);
+begin
+    votes <= head_ref & ref_1 & ref_2 & ref_3;
+
+    -- Count the number of '1's
+  process(votes)
+     begin
+        vote_count <= to_integer(unsigned(votes(0))) +
+                     to_integer(unsigned(votes(1))) +
+                     to_integer(unsigned(votes(2))) +
+                     to_integer(unsigned(votes(3)));
+     end process;
+
+
+    -- Alternatively, manually count
+    -- 
+    -- Determine the outcome using with-select
+    with vote_count select
+        vote <= '1' when 3 | 4,                -- Majority '1'
+                head_ref when 2,               -- Tie: head_ref's vote
+                '0' when 0 | 1,                -- Majority '0'
+                'X' when others;               -- Undefined (shouldn't occur)
+end Behavioral;
+  
+ 
     
