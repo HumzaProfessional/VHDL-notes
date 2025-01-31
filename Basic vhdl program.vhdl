3f080a0d7a17a7873615349efe5873e0f61f3cd5
Humza Rana <142959387+HumzaProfessional@users.noreply.github.com>
2025-01-30 21:35:25 -0500

    Update Basic vhdl program.vhdl

--- Basic vhdl program.vhdl
+++ Basic vhdl program.vhdl
@@ -3,13 +3,20 @@ use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.STD_LOGIC_ARITH.ALL;
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
-entity AND2 is
-    Port ( A : in STD_LOGIC;
-           B : in STD_LOGIC;
-           Y : out STD_LOGIC);
-end AND2;
+entity Intro is
+port (
+    sw1: in std_logic;
+    sw2: in std_logic;
+    sw3: in std_logic;
+    led1: out std_logic;
+    led2: out std_logic;
+    led3: out std_logic
+);
+end Intro;
 
-architecture Behavioral of AND2 is
+architecture Intro_ARCH of Intro is
 begin
-    Y <= A AND B;
-end Behavioral;
+led1 <= sw1 or sw2 or sw3;
+led2 <= (sw1 and sw2) or (sw1 and sw3) or (sw2 and sw3);
+led3 <= sw1 and sw2 and sw3;
+end Intro_ARCH;
