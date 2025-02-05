78b6b1cbb86033310791f622cf381b46cafa0a78
Humza Rana <142959387+HumzaProfessional@users.noreply.github.com>
2025-02-05 07:36:36 -0500

    Update Vote_selector_tb.vhdl

--- lab1/Vote_selector_tb.vhdl
+++ lab1/Vote_selector_tb.vhdl
@@ -1,13 +1,13 @@
 library ieee;
 use IEEE.STD_LOGIC_1164.ALL;
 
-entity Voter_TB is
+entity Voter_TB is -- entity is not needed
 end Voter_TB;
 
-architecture Voter_TB_ARCH of Voter_TB is 
+architecture Voter_TB_ARCH of Voter_TB is -- declaration of port
   component Voter
   port (
-        signal head_ref: in std_logic;
+        signal head_ref: in std_logic; 
         signal ref_1 : in std_logic;
         signal ref_2 : in std_logic;
         signal ref_3: in std_logic;
@@ -15,14 +15,14 @@ architecture Voter_TB_ARCH of Voter_TB is
     );
   end component;
 
-  signal head_ref : std_logic;
+  signal head_ref : std_logic; -- declarations of signals
   signal ref_1 : std_logic;
   signal ref_2 : std_logic;
   signal ref_3 : std_logic;
   signal vote: std_logic;
 
 begin 
-  UUT: Voter port map(
+  UUT: Voter port map( -- delcaration of inputs and outputs
     head_ref => head_ref,
     ref_1 => ref_1,
     ref_2 => ref_2,
@@ -30,9 +30,9 @@ begin
     vote => vote
     );
 
-SWITCH_DRIVER: process
+SWITCH_DRIVER: process -- where akk possible outputs are decided
 begin
-    head_ref <= '0';
+    head_ref <= '0'; -- all votes are 0, so the vector will be "0000"
     ref_1 <= '0';
     ref_2 <= '0';
     ref_3 <= '0';
