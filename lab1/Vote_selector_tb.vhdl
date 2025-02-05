b779e4fb0e803ce832fa420ad4ef75c113f39dcf
Humza Rana <142959387+HumzaProfessional@users.noreply.github.com>
2025-02-05 11:36:42 -0500

    Update Vote_selector_tb.vhdl

--- lab1/Vote_selector_tb.vhdl
+++ lab1/Vote_selector_tb.vhdl
@@ -7,110 +7,110 @@ end Voter_TB;
 architecture Voter_TB_ARCH of Voter_TB is -- declaration of port
   component Voter
   port (
-        signal head_ref: in std_logic; 
-        signal ref_1 : in std_logic;
-        signal ref_2 : in std_logic;
-        signal ref_3: in std_logic;
-        signal vote : out std_logic
+        signal headref: in std_logic; 
+        signal ref1 : in std_logic;
+        signal ref2 : in std_logic;
+        signal ref3: in std_logic;
+        signal finalvote : out std_logic
     );
   end component;
 
-  signal head_ref : std_logic; -- declarations of signals
-  signal ref_1 : std_logic;
-  signal ref_2 : std_logic;
-  signal ref_3 : std_logic;
-  signal vote: std_logic;
+  signal headref : std_logic; -- declarations of signals
+  signal ref1 : std_logic;
+  signal ref2 : std_logic;
+  signal ref3 : std_logic;
+  signal finalvote: std_logic;
 
 begin 
   UUT: Voter port map( -- delcaration of inputs and outputs
     head_ref => head_ref,
-    ref_1 => ref_1,
-    ref_2 => ref_2,
-    ref_3 => ref_3,
-    vote => vote
+    ref1 => ref_1,
+    ref2 => ref_2,
+    ref3 => ref_3,
+    finalvote => vote
     );
 
 SWITCH_DRIVER: process -- where akk possible outputs are decided
 begin
-    head_ref <= '0'; -- all votes are 0, so the vector will be "0000"
-    ref_1 <= '0';
-    ref_2 <= '0';
-    ref_3 <= '0';
+    headref <= '0'; -- all votes are 0, so the vector will be "0000"
+    ref1 <= '0';
+    ref2 <= '0';
+    ref3 <= '0';
     wait for 20 ns;
-    head_ref <= '1';
-    ref_1 <= '0';
-    ref_2 <= '0';
-    ref_3 <= '0';
+    headref <= '1';
+    ref1 <= '0';
+    ref2 <= '0';
+    ref3 <= '0';
     wait for 20 ns;
-    head_ref <= '0';
-    ref_1 <= '1';
-    ref_2 <= '0';
-    ref_3 <= '0';
+    headref <= '0';
+    ref1 <= '1';
+    ref2 <= '0';
+    ref3 <= '0';
     wait for 20 ns;
-   head_ref <= '1';
-    ref_1 <= '1';
-    ref_2 <= '0';
-    ref_3 <= '0';
+   headref <= '1';
+    ref1 <= '1';
+    ref2 <= '0';
+    ref3 <= '0';
     wait for 20 ns;
-    head_ref <= '0';
-    ref_1 <= '0';
-    ref_2 <= '1';
-    ref_3 <= '0';
+    headref <= '0';
+    ref1 <= '0';
+    ref2 <= '1';
+    ref3 <= '0';
     wait for 20 ns;
-    head_ref <= '1';
-    ref_1 <= '0';
-    ref_2 <= '1';
-    ref_3 <= '0';
+    headref <= '1';
+    ref1 <= '0';
+    ref2 <= '1';
+    ref3 <= '0';
     wait for 20 ns;
-    head_ref <= '0';
-    ref_1 <= '1';
-    ref_2 <= '1';
-    ref_3 <= '0';
+    headref <= '0';
+    ref1 <= '1';
+    ref2 <= '1';
+    ref3 <= '0';
     wait for 20 ns;
-    head_ref <= '1';
-    ref_1 <= '1';
-    ref_2 <= '1';
-    ref_3 <= '0';
+    headref <= '1';
+    ref1 <= '1';
+    ref2 <= '1';
+    ref3 <= '0';
     wait for 20 ns;
-    head_ref <= '0';
-    ref_1 <= '0';
-    ref_2 <= '0';
-    ref_3 <= '1';
+    headref <= '0';
+    ref1 <= '0';
+    ref2 <= '0';
+    ref3 <= '1';
     wait for 20 ns;
-    head_ref <= '1';
-    ref_1 <= '0';
-    ref_2 <= '0';
-    ref_3 <= '1';
+    headref <= '1';
+    ref1 <= '0';
+    ref2 <= '0';
+    ref3 <= '1';
     wait for 20 ns;
-    head_ref <= '0';
-    ref_1 <= '1';
-    ref_2 <= '0';
-    ref_3 <= '1';
+    headref <= '0';
+    ref1 <= '1';
+    ref2 <= '0';
+    ref3 <= '1';
     wait for 20 ns;
-    head_ref <= '1';
-    ref_1 <= '1';
-    ref_2 <= '0';
-    ref_3 <= '1';
+    headref <= '1';
+    ref1 <= '1';
+    ref2 <= '0';
+    ref3 <= '1';
     wait for 20 ns;
-    head_ref <= '0';
-    ref_1 <= '0';
-    ref_2 <= '1';
-    ref_3 <= '1';
+    headref <= '0';
+    ref1 <= '0';
+    ref2 <= '1';
+    ref3 <= '1';
     wait for 20 ns;
-    head_ref <= '1';
-    ref_1 <= '0';
-    ref_2 <= '1';
-    ref_3 <= '1';
+    headref <= '1';
+    ref1 <= '0';
+    ref2 <= '1';
+    ref3 <= '1';
     wait for 20 ns;
-    head_ref <= '0';
-    ref_1 <= '1';
-    ref_2 <= '1';
-    ref_3 <= '1';
+    headref <= '0';
+    ref1 <= '1';
+    ref2 <= '1';
+    ref3 <= '1';
     wait for 20 ns;
-    head_ref <= '1';
-    ref_1 <= '1';
-    ref_2 <= '1';
-    ref_3 <= '1';
+    headref <= '1';
+    ref1 <= '1';
+    ref2 <= '1';
+    ref3 <= '1';
     wait; --suspend process forever
 end process;
 
